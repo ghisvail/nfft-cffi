01f165a826cde563d831b5a26598c4ebeb504820
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-02-04 19:43:20 +0000

    Add enum34 install dependency for python < 3.4

--- setup.py
+++ setup.py
@@ -9,10 +9,19 @@
 from setuptools import find_packages, setup
 
 
+def get_install_requires():
+    from distutils.version import StrictVersion
+    from sys import version_info
+    install_requires = ['cffi>=1.0.0', 'numpy']
+    py_version = StrictVersion('.'.join(str(n) for n in version_info[:3]))
+    if py_version < StrictVersion('3.4'):
+        install_requires.append('enum34')
+
+
 setup(
     packages=find_packages(exclude=['builders', 'docs', 'tests']),
     setup_requires=['cffi>=1.0.0', 'pkgconfig'],
-    install_requires=['cffi>=1.0.0', 'numpy'],
+    install_requires=get_install_requires(),
     test_requires=['nose'],
     ext_package='nfft',
     cffi_modules=['builders/build_bindings.py:ffi'],
