10cc5849c9145728efc6e76a9b18c22cb1e617dd
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-02-09 23:50:46 +0000

    Fixup copyright headers.

--- nfft/nfft.py
+++ nfft/nfft.py
@@ -2,9 +2,9 @@
 # Copyright (c) 2016, Ghislain Antony Vaillant
 # All rights reserved.
 #
-# Distributed under the terms of the new BSD license.
-# See the accompanying LICENSE file or read the terms at
-# https://opensource.org/licenses/BSD-3-Clause.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the BSD license. See the accompanying LICENSE file
+# or read the terms at https://opensource.org/licenses/BSD-3-Clause.
 
 from __future__ import absolute_import
 
