d14582c5feb1a0c0d8e2fd9117a7f30cfd7220b1
Ghislain Antony Vaillant <ghisvail@gmail.com>
2017-06-03 15:41:16 +0100

    More copyright updates

--- nfft/__init__.py
+++ nfft/__init__.py
@@ -1,10 +1,10 @@
-# Copyright (c) 2016, Imperial College London
-# Copyright (c) 2016, Ghislain Antony Vaillant
+# Copyright (c) 2016-2017, Imperial College London
+# Copyright (c) 2016-2017, Ghislain Antony Vaillant
 # All rights reserved.
 #
-# Distributed under the terms of the new BSD license.
-# See the accompanying LICENSE file or read the terms at
-# https://opensource.org/licenses/BSD-3-Clause.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the BSD license. See the accompanying LICENSE file
+# or read the terms at https://opensource.org/licenses/BSD-3-Clause.
 
 from pkg_resources import get_distribution, DistributionNotFound
 
